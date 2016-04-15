6058b28931cd3f1faa547a58a1b34c9d6b3a6d19
ClintonTak <clintontak22@live.com>
2016-04-15 09:03:38 -0400

    sql stuff
    Added the create table for user and the insert into events and users sql

---
+++ SQL/createuserinsertion.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (FIRSTNAME, LASTNAME, USERNAME, PASSWORD, PRIV)
+VALUES ($firstname, $lastname, $password, $priv);
+
+INSERT INTO events (ID, NAME, GROUP, LOCATION, DESCRIPTION, ) 
+VALUES ($id, $eventName, $group, $location, $description, )
