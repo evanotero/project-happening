7aa82b181e2d4484d437bfae1cdce7a1af9c02a7
evanotero <oteroev@bc.edu>
2016-04-25 16:19:41 -0400

    Added script to support Bootstrap 3 Datepicker v4

--- js/scripts.js
+++ js/scripts.js
@@ -73,3 +73,15 @@ $(function() {
         }, $msgShowTime);
     }
 });
+$(function() {
+    $('#datetimepicker1').datetimepicker();
+    $('#datetimepicker2').datetimepicker({
+        useCurrent: false //Important! See issue #1075
+    });
+    $("#datetimepicker1").on("dp.change", function(e) {
+        $('#datetimepicker2').data("DateTimePicker").minDate(e.date);
+    });
+    $("#datetimepicker7").on("dp.change", function(e) {
+        $('#datetimepicker1').data("DateTimePicker").maxDate(e.date);
+    });
+});
