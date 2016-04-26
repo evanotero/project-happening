d5321b200403f297e6a6018f36d5fcada1f73666
evanotero <oteroev@bc.edu>
2016-04-26 12:43:17 -0400

    Initial Commit of PHP file to verify Google reCaptcha

---
+++ includes/verifycaptcha.php
@@ -0,0 +1,27 @@
+<?php
+  $response = array();
+  $errMsg = ""; 
+  if(isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response'])) {
+    // Secret Key
+    $secret = '6LciMB4TAAAAAHC62dLUopifKuaJlF6XT1kLKPcZ';
+    // Verify Response Data
+    $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$secret.'&response='.$_POST['g-recaptcha-response']);
+    $responseData = json_decode($verifyResponse);
+    if($responseData->success) {
+      // Do nothing...
+    } else {
+      $errMsg = 'Robot verification failed, please try again.';
+    }
+  } else {
+    $errMsg = 'Please click on the reCAPTCHA box.';
+  }
+
+  if($errMsg) {
+    $response['status'] = 'failure';
+    $response['errormsg'] = $errMsg;
+  } else
+    $response['status'] = 'success';
+  header('Content-type: application/json');
+  echo json_encode($response);
+}
+?>
