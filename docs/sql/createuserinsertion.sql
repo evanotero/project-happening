635627a515de69aab4c45ef65d3074c8f92350f1
evanotero <oteroev@bc.edu>
2016-04-27 13:25:52 -0400

    Removed file
    User insertions moved to tables.sql

--- docs/sql/createuserinsertion.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO users (FIRSTNAME, LASTNAME, USERNAME, PASSWORD, PRIV)
-VALUES ($firstname, $lastname, $password, $priv);
-
-INSERT INTO events (ID, NAME, GROUP, LOCATION, DESCRIPTION, ) 
-VALUES ($id, $eventName, $group, $location, $description, )
