4de79dbe6abb1bec56f969f2dbe6e769f10ca557
evanotero <oteroev@bc.edu>
2016-04-29 16:07:42 -0400

    Added full functionality for fetching events

--- includes/fetchevents.php
+++ includes/fetchevents.php
@@ -1,6 +1,5 @@
 <?php
 include("dbconn.php");
-
 // Get RSS URL
 $URL = $_POST['url'];
 $pages = $_POST['per_page'];
@@ -19,40 +18,40 @@ try {
 } catch (Exception $e) {
    echo 'Caught exception: ',  $e->getMessage(), "\n";
 }
-//$dbc = connect_to_db("insertname"); // Connect to DB
+$dbc = connect_to_db("takc"); // Connect to DB
 $items = $rssobject->channel->item; // One or the other will work
 if (!$items) $items = $rssobject->item;
 foreach ($items as $item) {
-    $name = $item->title;
-    $group = $item->children("event", true)->organizer[0];
-    $location = $item->children("event", true)->location[0];
-    $description = $item->description;
-    $startdate = $item->children("event", true)->startdate[0];
-    $enddate = $item->children("event", true)->enddate[0];
-    $type = $item->children("event", true)->type[0];
-    $mediaurl = isset($item->children("media", true)->content) ? $item->children("media", true)->content->attributes()["url"] : NULL;
-    $eventlink = $item->link;
-    // 1. Query to see if event already exits in database
-    $val = mysql_query('select NAME from `events` where NAME == $name');
-    if($val === FALSE)  // 3. Convert XML date to SQL date:I think the current format is fine
-    {                    // 4. Insert Event into DB
-        $query = "INSERT INTO events (
-                    ID, NAME,ORGANIZER, LOCATION,DESCRIPTION, 
-                    MEDIAURL,STARTDATE, ENDDATE,LINK, APPROVED) 
-                    VALUES ('', '$name','$group','$location','$description','$mediaurl',
-                    '$startdate','$enddate','$type','$eventlink')";
-        $dbc = connect_to_db("takc");
-        $result = perform_query( $dbc, $query); 
-       
+    $name = mysqli_real_escape_string($dbc ,$item->title);
+    $group = mysqli_real_escape_string($dbc, $item->children("event", true)->organizer[0]);
+    $location = mysqli_real_escape_string($dbc, $item->children("event", true)->location[0]);
+    $description = mysqli_real_escape_string($dbc, $item->description);
+    $startdate = mysqli_real_escape_string($dbc, $item->children("event", true)->startdate[0]);
+    $enddate = mysqli_real_escape_string($dbc, $item->children("event", true)->enddate[0]);
+    if (isset($item->children("media", true)->content)) {
+        $atts = $item->children("media", true)->content->attributes();
+        $mediaurl = mysqli_real_escape_string($dbc, $atts["url"]);
+    } else {
+        $mediaurl = NULL;
     }
-    // $result = perform_query( $dbc, $query )
-    //print_r ($item); DEBUG
+    $eventlink = mysqli_real_escape_string($dbc, $item->link);
+    // Query to see if event already exits in database
+    $query = "select NAME from events where NAME = '$name'";
 
+    $result = perform_query( $dbc, $query);
+    if (mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+        // Event already exists with this name
+    } else {                  
+        // Insert Event into DB
 
-    //insert
-    
+        $query = "INSERT INTO events (
+                    NAME, ORGANIZER, LOCATION, DESCRIPTION, 
+                    MEDIAURL, STARTDATE, ENDDATE, LINK, APPROVED, U_ID) 
+                    VALUES ('$name','$group','$location','$description','$mediaurl',
+                    '$startdate','$enddate', '$eventlink', 1, 1)";
+
+        $result = perform_query( $dbc, $query); 
+    }    
 }
-
-
-// disconnect_from_db($dbc, $result);
+disconnect_from_db( $dbc, $result );
 ?>
