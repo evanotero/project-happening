4adeafb155ca1d8dce95d6730714ecb4fc1f3e40
evanotero <oteroev@bc.edu>
2016-05-01 19:21:41 -0400

    Removed variable

--- includes/display.php
+++ includes/display.php
@@ -1,17 +1,18 @@
 <?php
 include("dbconn.php");
+$string = isset($_GET['q']) ? $_GET['q'] : "";
 $dbc= connect_to_db("takc");
 if ($string == "" ) {
     //$query="select * from `events`";
-    $query="select * from `events` where `name` NOT IN ($list) ";
+    $query="select * from `events` where `name`";
 } else {
     $string = "%".$string."%";
     $query = "(select * from `events` where `name` NOT IN ($list) AND `NAME` like '$string')  union
-                            (select * from `events` where `GROUP` like '$string' AND `attraction_id` NOT IN ($list)) union
-                            (select * from `events` where `LOCATION` like '$string' AND `attraction_id` NOT IN ($list)) union
-                            (select * from `events` where `STARTDATE` like '$string' AND `attraction_id` NOT IN ($list))";
+                            (select * from `events` where `GROUP` like '$string' AND `attraction_id`) union
+                            (select * from `events` where `LOCATION` like '$string' AND `attraction_id`) union
+                            (select * from `events` where `STARTDATE` like '$string' AND `attraction_id`)";
 }
-$result = mysqli_query($dbc, $query) or die("bad query".mysqli_error($dbc));          
+$result = perform_query( $dbc, $query );       
 $data = array();
 while ($obj = mysqli_fetch_object($result)) {
     $data[] = $obj;
