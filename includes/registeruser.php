35ab7f28c7841e6f151f481b35dc40622e1eafa1
evanotero <oteroev@bc.edu>
2016-05-03 17:27:58 -0400

    Shaw to sha

--- includes/registeruser.php
+++ includes/registeruser.php
@@ -6,8 +6,8 @@ $Username=$_POST['username'];
 $Register_email=$_POST['email'];
 $Password1=$_POST['password'];
 
-$PWEncrypt = shaw1($Password1);
-$dbc= connect_to_db("takc");
+$PWEncrypt = sha1($Password1);
+$dbc = connect_to_db("takc");
 
 
 $insertsql = "INSERT INTO users (FIRSTNAME, LASTNAME, USERNAME, EMAIL, PASSWORD, PRIV) VALUES ('$Firstname','$Lastname','$Username', '$Register_email','$PWEncrypt', 'unverified')";
