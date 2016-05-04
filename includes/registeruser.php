a267ee3f67d390fe8486e25d915b5a34232e3eba
evanotero <oteroev@bc.edu>
2016-05-03 20:25:17 -0400

    Added support for checking for multiple users

--- includes/registeruser.php
+++ includes/registeruser.php
@@ -6,16 +6,21 @@ $Firstname = $_POST['firstname'];
 $Lastname = $_POST['lastname'];
 $Username=$_POST['username'];
 $Register_email=$_POST['email'];
-$Password1=$_POST['password'];
-
-$PWEncrypt = sha1($Password1);
+$pwd=$_POST['password'];
 
 $dbc= connect_to_db("takc");
 
-// Need to check if user already exists
+// Check if user already exists
+$query = "SELECT * FROM users WHERE EMAIL='$Register_email' OR USERNAME='$Username';";
+$result = perform_query($dbc, $query);
 
-$insertsql = "INSERT INTO users (FIRSTNAME, LASTNAME, USERNAME, EMAIL, PASSWORD, PRIV) VALUES ('$Firstname','$Lastname','$Username', '$Register_email','$PWEncrypt', 'unverified');";
-$result = perform_query( $dbc, $query);
+if (mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+    $msg .= "exists";
+} else {
+    $query = "INSERT INTO users (FIRSTNAME, LASTNAME, USERNAME, EMAIL, PASSWORD, PRIV) VALUES ('$Firstname','$Lastname','$Username', '$Register_email', sha1('$pwd'), 'unverified');";
+    $result = perform_query( $dbc, $query);
+    $msg .= "success";
+}
 echo $msg;
 disconnect_from_db( $dbc, $result );
 ?>
