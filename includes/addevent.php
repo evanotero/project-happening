ac8e90f0b1bc700efff0b2f7e5695fb521a1ff35
sinclairjones95 <jonesuz@bc.edu>
2016-05-04 20:21:43 -0400

    update

--- includes/addevent.php
+++ includes/addevent.php
@@ -21,29 +21,31 @@ $dbc= connect_to_db("takc");
 $query = "SELECT U_ID, PRIV from users where USERNAME='".$username."' AND PASSWORD='".$password."';";
 
 $result = perform_query($dbc, $query);
-
-if(!(mysqli_num_rows($result) == 0)){
+/*************Checks to see if the user is registered***************/
+if(!(mysqli_num_rows($result) == 0)){   //user exists
 
 	while($obj = $result->fetch_object()) {   //assigning values for user
 	
-		$U_ID = $obj->U_ID;
-		$priv = $obj->PRIV;
+		$U_ID = $obj->U_ID;   //user id
+		$priv = $obj->PRIV;   //user privacy ('admin', 'user', 'unverified')
 
 
 	}
-	if ($priv='unverified'){  // not verified user
+	if ($priv='unverified'){  //****** not verified user
 		$msg .= "Your user has not yet been verified";
 	}
-	else{
+	else {                   //******* Either an 'admin' or 'user'
 		// Check if link already exists & 
 		//if event w/ that name on that day
 		$query = "SELECT * FROM events WHERE LINK='$Link' OR EVENTNAME='$EventName' AND STARTDATE = '$Startdate';";
 		$result = perform_query($dbc, $query);
 
+		/**********Checks if the event already exists**********/
 		if (mysqli_fetch_array($result, MYSQLI_ASSOC)) {
     		$msg .= "event already exists";
-		} else {
+		} else {         //******the event does not exist
 			$verifed=0;
+			/*******if the user is an admin then the event is verified*******/
 			if ($priv = 'admin')
 				$verified = 1;
 			else        //user
@@ -52,12 +54,12 @@ if(!(mysqli_num_rows($result) == 0)){
     		$query = "insert into events 
     			(NAME, ORGANIZER, LOCATION, DESCRIPTION, MEDIAURL, STARTDATE, ENDDATE, LINK, APPROVED, U_ID) values 
     			('$EventName', '$GroupName', '$Location', '$Description', '$Media', '$Startdate', '$Enddate', '$Link', $verified, $U_ID);";
-   		 $result = perform_query( $dbc, $query);
+   			$result = perform_query( $dbc, $query);
     		$msg .= "success";
 		}
 	}
-}
-else {         //you are not a user
+}     
+else {         //**********User does not exist
 	$msg .="You are not a user";
 }
 	echo $msg;
