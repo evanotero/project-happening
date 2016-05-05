28350816db1ee433e69c34f8d8419a0d0d9740bd
sinclairjones95 <jonesuz@bc.edu>
2016-05-04 20:04:42 -0400

    create events
    adds events depending on user’s privacy

--- includes/addevent.php
+++ includes/addevent.php
@@ -11,23 +11,56 @@ $Startdate = $_POST['datestart'];
 $Enddate = $_POST['dateend'];
 $Description = $_POST['description'];
 
+$username = $_POST['username'];
+$pass = $_POST['password'];
+
+$password = sha1($pass);
+
 $dbc= connect_to_db("takc");
 
-// Check if link already exists & 
-//if event w/ that name on that day
-$query = "SELECT * FROM events WHERE LINK='$Link' OR EVENTNAME='$EventName' AND STARTDATE = '$Startdate';";
+$query = "SELECT U_ID, PRIV from users where USERNAME='".$username."' AND PASSWORD='".$password."';";
+
 $result = perform_query($dbc, $query);
 
-if (mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-    $msg .= "exists";
-} else {
-	//check user id and privledges, if admin approved = 1, if user approved = 0
-    $query = "insert into events 
-    	(NAME, ORGANIZER, LOCATION, DESCRIPTION, MEDIAURL, STARTDATE, ENDDATE, LINK, APPROVED, U_ID) values 
-    	('$EventName', '$GroupName', '$Location', '$Description', '$Media', '$Startdate', '$Enddate', '$Link', 0, );";
-    $result = perform_query( $dbc, $query);
-    $msg .= "success";
+if(!(mysqli_num_rows($result) == 0)){
+
+	while($obj = $result->fetch_object()) {   //assigning values for user
+	
+		$U_ID = $obj->U_ID;
+		$priv = $obj->PRIV;
+
+
+	}
+	if ($priv='unverified'){  // not verified user
+		$msg .= "Your user has not yet been verified";
+	}
+	else{
+		// Check if link already exists & 
+		//if event w/ that name on that day
+		$query = "SELECT * FROM events WHERE LINK='$Link' OR EVENTNAME='$EventName' AND STARTDATE = '$Startdate';";
+		$result = perform_query($dbc, $query);
+
+		if (mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+    		$msg .= "event already exists";
+		} else {
+			$verifed=0;
+			if ($priv = 'admin')
+				$verified = 1;
+			else        //user
+				$verified = 0;
+			//check user id and privledges, if admin approved = 1, if user approved = 0
+    		$query = "insert into events 
+    			(NAME, ORGANIZER, LOCATION, DESCRIPTION, MEDIAURL, STARTDATE, ENDDATE, LINK, APPROVED, U_ID) values 
+    			('$EventName', '$GroupName', '$Location', '$Description', '$Media', '$Startdate', '$Enddate', '$Link', $verified, $U_ID);";
+   		 $result = perform_query( $dbc, $query);
+    		$msg .= "success";
+		}
+	}
 }
-echo $msg;
-disconnect_from_db( $dbc, $result );
+else {         //you are not a user
+	$msg .="You are not a user";
+}
+	echo $msg;
+
+	disconnect_from_db( $dbc, $result );
 ?>
