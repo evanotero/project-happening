5be679e8b2120d978deec3105546556c9df38ca3
evanotero <oteroev@bc.edu>
2016-05-31 23:11:53 -0400

    Added support for Full Text Searching

--- includes/display.php
+++ includes/display.php
@@ -12,10 +12,18 @@ $enddate = mysqli_real_escape_string($dbc, $enddate);
 if ($string == "") {
     $query = "SELECT * FROM `events` WHERE `STARTDATE` BETWEEN '$startdate' AND '$enddate' ORDER BY `STARTDATE` ASC;";
 } else {
-    $string = "%".$string."%";
-    $query = "(select * from `events` where `NAME` like '$string' and STARTDATE >= '$startdate') union (select * from `events` where `ORGANIZER` like '$string' and STARTDATE >= '$startdate') union (select * from `events` where `LOCATION` like '$string' and STARTDATE >= '$startdate') union (select * from `events` where `DESCRIPTION` like '$string' and STARTDATE >= '$startdate') ORDER BY `STARTDATE` ASC;";
+    // $string = "%".$string."%";
+    // $query = "(select * from `events` where `NAME` like '$string' and STARTDATE >= '$startdate') union (select * from `events` where `ORGANIZER` like '$string' and STARTDATE >= '$startdate') union (select * from `events` where `LOCATION` like '$string' and STARTDATE >= '$startdate') union (select * from `events` where `DESCRIPTION` like '$string' and STARTDATE >= '$startdate') ORDER BY `STARTDATE` ASC;";
+    // $string = mysql_real_escape_string($string);  AND `STARTDATE` >= '$startdate' ORDER BY `STARTDATE` ASC;  WITH QUERY EXPANSION
+    $query = "SELECT * FROM `events` WHERE MATCH (`NAME`,`ORGANIZER`,`LOCATION`,`DESCRIPTION`) AGAINST ('$string' IN NATURAL LANGUAGE MODE) AND `STARTDATE` >= '$startdate' LIMIT 50;";
 }
-$result = perform_query( $dbc, $query );       
+$result = perform_query( $dbc, $query );
+
+if (mysqli_num_rows($result) == 0 && $string != "") {
+    $query = "SELECT * FROM `events` WHERE MATCH (`NAME`,`ORGANIZER`,`LOCATION`,`DESCRIPTION`) AGAINST ('$string' IN NATURAL LANGUAGE MODE WITH QUERY EXPANSION) AND `STARTDATE` >= '$startdate' LIMIT 50;";
+    $result = perform_query( $dbc, $query );
+}
+
 $data = array();
 while ($obj = mysqli_fetch_object($result)) {
     $data[] = $obj;
